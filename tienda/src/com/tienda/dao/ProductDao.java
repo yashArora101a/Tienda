1c096565d764f7f974471af46102aed0813ac728
YAro <YAro@DESKTOP-2NRGNH9>
2019-04-26 22:34:55 +0530

    add jars

---
+++ tienda/src/com/tienda/dao/ProductDao.java
@@ -0,0 +1,7 @@
+package com.tienda.dao;
+import org.springframework.orm.jpa.JpaTemplate;  
+import org.springframework.transaction.annotation.Transactional;
+
+public class ProductDao {
+	
+}
---
+++ tienda/src/com/tienda/resource/ProductResource.java
@@ -0,0 +1,32 @@
+package com.tienda.resource;
+
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.tienda.bean.Product;
+import com.tienda.service.TiendaService;
+
+
+@Path("/products")
+public class ProductResource {
+	/*@GET
+	@Produces(value={MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public Set<Product> fetchAllOrdersForUser(@PathParam("uname") String username) {
+		Set<Product> products=null;
+		products=new TiendaService().getAllProducts();
+		return products;
+		
+	}*/
+	@POST
+	@Consumes(value={MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public Response addProduct(Product product) {
+		System.out.print("11111111111111111yash");
+		System.out.print(product);
+		new TiendaService().addProduct(product);
+		return Response.ok().build();
+	}
+}
